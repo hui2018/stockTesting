c65d88c06642bc05be60716945264ccf89144508
Jack <hui2@pdx.edu>
2020-04-25 19:22:29 -0700

    Try printing out the data from json to manipulate the data

--- Main.py
+++ Main.py
@@ -2,9 +2,31 @@ from alpha_vantage.timeseries import TimeSeries
 from pprint import pprint
 import pandas
 
-ts = TimeSeries(key='YOUR_API_KEY', output_format='pandas')
-data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
-pprint(data.head(2))
+
+ts = TimeSeries(key='1AEF9PXLWXI8NDQ2', output_format='pandas')
+
+#print interval
+data, meta_data = ts.get_intraday(symbol='MSFT',interval='5min', outputsize='full')
+#pprint(data.head(5))
+
+#search
+'''
+symbol = "BA"
+test = ts.get_symbol_search(symbol)
+pprint(test)
+'''
+
+#print dividend amount
+'''
+check = ts.get_daily_adjusted(outputsize='compact', symbol='BA')
+pprint(check)
+'''
+
+list = ts.get_quote_endpoint(symbol='BA')
+for a in list:
+    print(a["Global Quote"]["02. open"])
+#print(list)
+
 
 '''
 ts = TimeSeries(key='1AEF9PXLWXI8NDQ2')
